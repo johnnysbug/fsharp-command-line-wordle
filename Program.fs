7017440e9c009d40c30fec612a59afeaf1a8ba68
Jonathan Moosekian <johnnysbug@gmail.com>
2022-02-21 22:31:45 -0600

    fixes matching same letter multiple times

--- Program.fs
+++ Program.fs
@@ -4,38 +4,44 @@ open System
 let word =
     File.ReadAllLines("words.txt")
     |> Array.filter (fun w -> w.Length = 5)
-    |> Seq.sortBy (fun _ -> Guid.NewGuid())
+    |> Seq.sortBy (fun _ -> Guid.NewGuid()) // small trick to get a random-ish word
     |> Seq.head
 
 let mutable results = [||]
 
-let draw ((guess: string), (word: string)) =
-    guess
-        |> Seq.mapi (fun i g -> 
-            match word.IndexOf(g) with
-            | w when w = i -> "🟩"
-            | w when w > -1 -> 
-                match word.IndexOf(g, i) with
-                | w when w = i -> "🟩"
-                | _ -> "🟨"
-            | _ -> "⬛️")
-        |> String.concat ""
+let matchMap index (letter: char) (word: string) =
+    match word.IndexOf(letter) with
+    | wordIndex when wordIndex = index -> "🟩"
+    | wordIndex when wordIndex > -1 -> 
+        match word.IndexOf(letter, index) with
+        | wordIndex when wordIndex = index -> "🟩"
+        | _ -> "🟨"
+    | _ -> "⬛️"
 
-let didWin (guess: string) (word: string) =
+let draw ((guess: string), (word: string)) =
+    let blocks = 
+        guess
+        |> Seq.distinct
+        |> Seq.mapi (fun i l -> matchMap i l word)
+        |> String.concat ""
+    blocks.PadRight(8, "⬛️"[0])
+
+let check (guess: string) (word: string) =
     match guess with
     | _ when guess = word -> 
         results <- Array.append results [| "🟩🟩🟩🟩🟩" |]
-        false
+        printfn "You win!"
+        true
     | _ when word.IndexOfAny (guess.ToCharArray()) > -1 -> 
         results <- Array.append results [| draw(guess, word) |]
-        true
+        false
     | _ -> 
         results <- Array.append results [| "⬛️⬛️⬛️⬛️⬛️" |]
-        true
+        false
 
 printfn "Guess the word!"
 
-let mutable tryAgain = true
-while tryAgain do
-    tryAgain <- didWin (Console.ReadLine()) word
+let mutable didWin = false
+while not didWin do
+    didWin <- check (Console.ReadLine()) word 
     results |> Array.iter (printfn "%s")
